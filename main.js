3dfa3a8597133110a14acfeb6d42c53af712ae6a
sebasolr <seba.olr@gmail.com>
2022-06-30 13:42:33 -0400

    estilo en css y funcionalidad en javascript

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
-let getUsers = fetch (`https://api.github.com/users/USERNAME`).then(function(response){
+let input = document.querySelector('.input');
+let botonAgregar= document.querySelector('.boton-agregar');
+let container = document.querySelector('.container');
 
+botonAgregar.addEventListener('click',function(){
+    console.log('hiciste click')
+    if(input.value ===('')){
+        console.log('agrega algo porfavor')
+    }else{
+        container.innerHTML= input.value
+    }
 })
+
+let Item = {
+    constructor(nuevaTarea){
+        this.crearDiv(nuevaTarea)
+    }
+}
