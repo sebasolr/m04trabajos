006be47d85c846aeca67401103fc0105eb201bde
sebasolr <seba.olr@gmail.com>
2022-07-01 13:04:25 -0400

    Funcionalidad en js, manipulacion DOM

--- main.js
+++ main.js
@@ -1,18 +1,58 @@
+//variables globales
 let input = document.querySelector('.input');
 let botonAgregar= document.querySelector('.boton-agregar');
 let container = document.querySelector('.container');
 
+// crear clase item
+class Item  {
+    constructor(crearDiv){
+       this.crearDiv
+    } 
+}
+//crea el input con todos sus atributos clasicos
+function crearDiv(tarea){
+    //crear div
+    const divnuevo= document.createElement('div')
+    //atributos del div creado
+    divnuevo.classList.add('item')
+    //crear input
+    const createInput= document.createElement('input')
+    //atributos del input
+    createInput.setAttribute('type','text');
+    createInput.setAttribute('disabled','true');
+    createInput.classList.add('item-input');
+    createInput.value= tarea;
+    //introducir el input dentro del div
+    divnuevo.appendChild(createInput);
+    //introducir el div dentro del container
+    container.appendChild(divnuevo);
+    //crear boton
+    const botonEliminar = document.createElement('button')
+    //atributos del boton remover
+    botonEliminar.classList.add('boton-remover')
+    botonEliminar.innerHTML=`<i class="fa-solid fa-pen-to-square"></i`
+    //agregar el boton al div
+    divnuevo.appendChild(botonEliminar)
+    //crear boton editar
+    const botonEditar = document.createElement('button')
+    //atributos del boton
+    botonEditar.classList.add('boton-editar')
+    botonEditar.innerHTML=`<i class="fa-solid fa-trash"></i>`
+    //agregar el boton al div
+    divnuevo.appendChild(botonEditar)
+}
+// evento agregar con el click
 botonAgregar.addEventListener('click',function(){
     console.log('hiciste click')
     if(input.value ===('')){
         console.log('agrega algo porfavor')
     }else{
-        container.innerHTML= input.value
+      crearDiv(input.value)
+      input.value =""
     }
 })
 
-let Item = {
-    constructor(nuevaTarea){
-        this.crearDiv(nuevaTarea)
-    }
-}
+
+//elemento html dOM
+//
+
