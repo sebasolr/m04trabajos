9cd85f9495b400dac638eaa98547c9e1719143ad
sebasolr <seba.olr@gmail.com>
2022-07-06 01:00:05 -0400

    arreglo del constructor, botones funcionales

--- main.js
+++ main.js
@@ -5,50 +5,63 @@ let container = document.querySelector('.container');
 
 // crear clase item
 class Item  {
-    constructor(crearDiv){
-       this.crearDiv
+    constructor(nueva_tarea){
+        this.crearDiv(nueva_tarea);
     } 
+    //metodos
+    crearDiv(tarea){
+        const newDiv = document.createElement('div')
+        newDiv.classList.add('item')
+        const newInput = document.createElement('input')
+        newInput.setAttribute('type','text');
+        newInput.setAttribute('disabled','true');
+        newInput.classList.add('item-input');
+        newInput.value= tarea;
+        
+        newDiv.appendChild(newInput);
+        container.appendChild(newDiv);
+        
+        const removeButton = document.createElement('button');
+        removeButton.classList.add('boton-remover')
+        removeButton.innerHTML='<i class="fa-solid fa-lock"></i>';
+        //agregar el boton al div
+        newDiv.appendChild(removeButton)
+        
+        const editButton = document.createElement('button')
+        //atributos del boton
+        editButton.classList.add('eeditar')
+        editButton.innerHTML='<i class="fa-solid fa-trash"></i>';
+        //agregar el boton al div
+        newDiv.appendChild(editButton)
+        
+        removeButton.addEventListener('click',function(){
+            if(removeButton.innerHTML === `<i class="fa-solid fa-lock-open"></i>` ){
+                removeButton.innerHTML=`<i class="fa-solid fa-lock"></i>`
+                newInput.setAttribute('disabled', 'true')
+            }else{
+                newInput.removeAttribute('disabled')
+                removeButton.innerHTML=`<i class="fa-solid fa-lock-open"></i>`
+            }
+        })
+        editButton.addEventListener('click',function(){
+            newDiv.remove('div')
+        })
+    }
 }
-//crea el input con todos sus atributos clasicos
-function crearDiv(tarea){
-    //crear div
-    const divnuevo= document.createElement('div')
-    //atributos del div creado
-    divnuevo.classList.add('item')
-    //crear input
-    const createInput= document.createElement('input')
-    //atributos del input
-    createInput.setAttribute('type','text');
-    createInput.setAttribute('disabled','true');
-    createInput.classList.add('item-input');
-    createInput.value= tarea;
-    //introducir el input dentro del div
-    divnuevo.appendChild(createInput);
-    //introducir el div dentro del container
-    container.appendChild(divnuevo);
-    //crear boton
-    const botonEliminar = document.createElement('button')
-    //atributos del boton remover
-    botonEliminar.classList.add('boton-remover')
-    botonEliminar.innerHTML=`<i class="fa-solid fa-pen-to-square"></i`
-    //agregar el boton al div
-    divnuevo.appendChild(botonEliminar)
-    //crear boton editar
-    const botonEditar = document.createElement('button')
-    //atributos del boton
-    botonEditar.classList.add('boton-editar')
-    botonEditar.innerHTML=`<i class="fa-solid fa-trash"></i>`
-    //agregar el boton al div
-    divnuevo.appendChild(botonEditar)
-}
+
+
+
 // evento agregar con el click
 botonAgregar.addEventListener('click',function(){
     console.log('hiciste click')
-    if(input.value ===('')){
+    if(input.value ===''){
         console.log('agrega algo porfavor')
     }else{
-      crearDiv(input.value)
-      input.value =""
+        const addItem = new Item(input.value);
+        
+        
+        input.value =""
+        
     }
 })
 
